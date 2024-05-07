16375ef6e7945759dafc774e875fff0744d39daf
Marquesse <145597991+Marquesse@users.noreply.github.com>
2024-05-07 13:21:11 -0400

    Add files via upload

---
+++ project01/project03-01.js
@@ -0,0 +1,34 @@
+/*    JavaScript 7th Edition
+      Chapter 3
+      Project 03-01
+
+      Application to calculate total order cost
+      Author: Mar'quesse Vaughn
+      Date:   05/07/2024
+
+      Filename: project03-01.js
+*/
+
+//variable for menu
+let menuItems = document.getElementsByClassName("menuItem");
+for(let i = 0; i < menuItems.length; i++) {
+menuItems[i].addEventListener("click", calcTotal);
+}
+        
+
+//Function for calcTotal
+function calcTotal(){ 
+let orderTotal = 0;
+          for(i = 0; i < 5; i++) {
+              if(menuItems[i].checked) {
+                orderTotal += (Number(menuItems[i].value) * 1);
+              }
+              document.getElementById("billTotal").innerHTML = formatCurrency(orderTotal);
+                 }
+                  };
+                         
+
+ // Function to display a numeric value as a text string in the format $##.## 
+ function formatCurrency(value) {
+    return "$" + value.toFixed(2);
+ }
